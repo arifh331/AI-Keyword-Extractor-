a43faab7b2141f11bd4f98831bc6463668cebfed
Arif Howlader <ah3288@nyu.edu>
2023-04-26 22:15:34 -0400

    Adding the api call body

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import React from 'react'
+import {useState} from 'react'
 import { Container, Box } from '@chakra-ui/react'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import TextInput from './components/TextInput'
+
+
 const App = () => {
-  const extractKeyWords = (text)=>{
+
+  const [keywords,setKeywords] = useState('')
+  const [isOpen,setIsOpen] = useState(false)
+  const [isLoading,setIsLoading] = useState(false)
+
+  const extractKeyWords = async (text)=>{
+    setIsLoading(true)
+    setIsOpen(true)
+
+    const option ={
+      method:'POST',
+      headers:{'Content-Type':'application/json',
+      Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+    
+    },
+    body:JSON.stringify({
+      model: `text-davinci-003`,
+      prompt: 'Extract keywords from this text. Make sure to make the first letter from each word uppercase and seperate with commas\n\n'
+       + text + '', 
+       temperature:0.6,
+       max_tokens:60,
+       frequency_penalty:0.8
+
+    })
+    }
+    const response = await 
     console.log(text)
   }
+
+  
 
 
 
